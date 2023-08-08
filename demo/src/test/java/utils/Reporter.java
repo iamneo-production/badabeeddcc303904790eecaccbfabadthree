e28e447faaa3cfb7a026c30246d9a34e11ab174a
Iamneo <techadmin@iamneo.ai>
2023-08-08 06:12:44 +0000

    Backup Commit

--- demo/src/test/java/utils/Reporter.java
+++ demo/src/test/java/utils/Reporter.java
@@ -1,8 +1,8 @@
 package utils;
-
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -23,22 +23,25 @@ public class Reporter {
         ExtentReports extentReport = new ExtentReports();
         PropsLoader propsLoader = PropsLoader.getInstance("/src/main/java/config.properties");
 
-        // Generate a timestamp for the report file
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timestampFormat = "dd/MM/yyyy hh:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(timestampFormat);
+        TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata");
+        dateFormat.setTimeZone(istTimeZone);
+
         String timestamp = dateFormat.format(new Date());
 
         // Define the file path with the timestamp
-        String reportFilePath = System.getProperty("user.dir") + "/src/main/reports/extentReport_" + timestamp + ".html";
+        String reportFilePath = System.getProperty("user.dir") + "/src/main/reports/extentReport.html";
         File extentReportFile = new File(reportFilePath);
 
         ExtentSparkReporter sparkReporter = new ExtentSparkReporter(extentReportFile);
-
         sparkReporter.config().setTheme(Theme.DARK);
         sparkReporter.config().setReportName("Gillette Test Report");
         sparkReporter.config().setDocumentTitle("Gillette Automation Automation Report");
-        sparkReporter.config().setTimeStampFormat("dd/MM/yyyy hh:mm:ss");
+        sparkReporter.config().setTimeStampFormat(timestampFormat); // Use the custom timestamp format
 
         extentReport.attachReporter(sparkReporter);
+        extentReport.setReportUsesManualConfiguration(true); 
 
         extentReport.setSystemInfo("Application URL", propsLoader.getProperty("url"));
         extentReport.setSystemInfo("Browser Name", propsLoader.getProperty("browserName"));
