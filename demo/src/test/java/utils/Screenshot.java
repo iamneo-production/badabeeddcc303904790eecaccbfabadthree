d19bf4e49156cb11254cf03e8b6a538153a5c8be
Iamneo <techadmin@iamneo.ai>
2023-08-08 08:03:01 +0000

    Backup Commit

--- demo/src/test/java/utils/Screenshot.java
+++ demo/src/test/java/utils/Screenshot.java
@@ -12,17 +12,27 @@ import org.openqa.selenium.io.FileHandler;
 
 public class Screenshot {
     public String captureScreenshot(WebDriver driver, String testName) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-		String timestamp = dateFormat.format(new Date());
-		File srcScreenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String destinationScreenshotPath = System.getProperty("user.dir")+"/src/main/screenshots/"+testName+"_"+timestamp+".png";
-		
-		try {
-			FileHandler.copy(srcScreenshot,new File(destinationScreenshotPath));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return destinationScreenshotPath;
-	}
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = dateFormat.format(new Date());
+        
+        // Define the screenshots directory path
+        String screenshotsDirectory = System.getProperty("user.dir") + "/src/main/screenshots/";
+        
+        // Create the screenshots directory if it doesn't exist
+        File directory = new File(screenshotsDirectory);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        
+        File srcScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String destinationScreenshotPath = screenshotsDirectory + testName + "_" + timestamp + ".png";
+
+        try {
+            FileHandler.copy(srcScreenshot, new File(destinationScreenshotPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return destinationScreenshotPath;
+    }
 }
