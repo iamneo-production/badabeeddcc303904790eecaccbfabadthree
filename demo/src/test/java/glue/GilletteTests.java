8c9c8f45ac541fff783739b1f1b2647a1a5dab27
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:00:16 +0000

    Backup Commit

--- demo/src/test/java/glue/GilletteTests.java
+++ demo/src/test/java/glue/GilletteTests.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
